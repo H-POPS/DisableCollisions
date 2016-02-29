c3537221db6e3eff3f37d50c987a68f58ac59f1d
H-POPS <henricopops@gmail.com>
2016-02-29 19:52:14 +0100

    Initialize

---
+++ DisableCollision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class DisableCollision : MonoBehaviour
+{
+    public void Disable()
+    {
+        List<Deco> list =  AssetManager.Instance.getDecoObjects().ToList();
+        foreach (Deco D in list)
+        {
+            try
+            {
+                foreach (BoundingVolume BV in D.GetComponents<BoundingVolume>())
+                {
+                    DestroyImmediate(BV);
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+        Debug.Log("Disabled Deco Collisions");
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace HelloMod
+{
+    public class Main : IMod
+    {
+        public string Identifier { get; set; }
+        GameObject _go; 
+        public void onEnabled()
+        {
+            _go = new GameObject();
+            DisableCollision DC = _go.AddComponent<DisableCollision>();
+            DC.Disable();
+        }
+
+        public void onDisabled()
+        {
+            UnityEngine.Object.Destroy (_go);
+        }
+
+        public string Name 
+        {
+            get { return "DislableCollisions"; }
+        }
+
+        public string Description
+        {
+            get { return "DislableCollisions"; }
+        }
+    }
+}
